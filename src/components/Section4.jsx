e7d2b0fbddd5b30a683b136f877039a80ef3ed90
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2025-04-09 20:55:13 +0800

    changes in section 4

---
+++ src/components/Section4.jsx
@@ -0,0 +1,16 @@
+export default function Section4() {
+  return (
+    <div className="m-8 space-y-8">
+      <div>
+        <h1 className="font-[PlayfairDisplay] text-[2rem] tracking-wide">
+          Education as a Tool for Nationalism: Rizal's Vision for the Filipino
+          People
+        </h1>
+        <p>
+          Rizal's contribution to education sector and vision of power through
+          education.
+        </p>
+      </div>
+    </div>
+  );
+}
