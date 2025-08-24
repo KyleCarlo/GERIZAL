607734e581933af7d9940cc7fffd118296e09d81
Kyle <kyle29.zero@gmail.com>
2025-08-25 01:22:22 +0800

    change base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/GERIZAL",
+  base: "/Rizal-Legacy-in-Philippine-Education",
   plugins: [react(), tailwindcss()],
 });
